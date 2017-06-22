b2ac0152b30e3086ee25b4cc5117356f94dd759a
Johaesun <991015a@naver.com>
2017-06-22 11:34:25 +0900

    170622

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Web developer &#124; Johysun</title>
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/font.css">
+</head>
+<body>
+    <div id="wrap">
+        <header>
+            <h1 class="logo"></h1>
+            <div class="nav"></div>
+        </header>
+        <div class="section">
+            <h2>JO_HYESUN</h2>
+            <h3>WEB DEVELOPER</h3>
+            <button>MY PORTFOLIO</button>
+        </div>
+        <footer>
+            <p>Copyright 2017 hyesun all rights reserved.</p>
+        </footer>
+    </div>
+</body>
+</html>
